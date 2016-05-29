0ccfc2235faa6b2cdf69c28a5602a5f561c8765a
Jesse Zbikowski <embeddedLinuxGuy@gmail.com>
2016-05-28 19:05:26 -0700

    fetch list of teams from Stattleship

--- mkdirs.sh
+++ mkdirs.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-mkdir -p img teams
-for f in $(<teams.txt); do
-  mkdir -p teams/$f
-  node teams.js $f
+mkdir -p img teams log
+#for f in $(<teams.txt); do
+    #mkdir -p teams/$f
+    #node teams.js $f
+
+# ln -s teams/{,} {}
+
+for f in $(<test/teams.txt); do
+    if [ ! -f data/mlb-$f-master.json ]; then
+	./get_logs.py $f > log/$f.out 2> log/$f.err
+	./team-filter.js $f
+    fi
 done
